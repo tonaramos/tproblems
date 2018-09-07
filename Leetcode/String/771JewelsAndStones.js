56bb67561cad19add8cc596584dd2403093bf7fe
Tona Ramos <gtonaramos@gmail.com>
2018-09-07 00:18:01 -0700

    Final Solution.

--- Leetcode/String/771JewelsAndStones.js
+++ Leetcode/String/771JewelsAndStones.js
@@ -15,38 +15,14 @@ Note:
 
 S and J will consist of letters and have length at most 50.
 The characters in J are distinct.
-
 */
 
-/*
- @param {string} J
- @param {string} S
- @return {number}
-*/
-
-// const numJewelsInStones = (J, S) => {
-//   let storage = {};
-//   J.split('').forEach(element => storage[element] = 0);
-//   S.split('').forEach(item => {   
-//     if (storage[item] || storage[item] === 0) {
-//       storage[item] += 1;
-//     }
-//   });
-//   return Object.keys(storage).map(item => storage[item]).reduce((accumulator, currentValue) => accumulator + currentValue);
-// }
-
-//============== Final code ==================
-//  compared to the attempt above where I iterated over the longer S string twice and had to produce an array with the storage values, 
-//    the code below iterates over S only once, and over J, which is shorter, multiple times. 
 let numJewelsInStones = (J, S) => {
-  let jewels = 0;                         
+  let jewels = 0;
   for (let i = 0; i < S.length; i++) {
-      if (J.indexOf(S[i]) !== -1) {       //  for indexOf method, if the value is not part of the J string the result is -1
+      if (J.indexOf(S[i]) !== -1) {
           jewels++;
       }
   }
   return jewels;
 };
-
-// console.log(numJewelsInStones('z','ZZ'));
-// console.log(numJewelsInStones('aA','aAAbbbb'));
