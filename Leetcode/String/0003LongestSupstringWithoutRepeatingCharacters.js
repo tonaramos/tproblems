77501e896a48ae66886073fae4d48fefe8f77743
tonaramos <gtonaramos@gmail.com>
2018-11-27 12:19:54 -0800

    Solution with comments.

--- Leetcode/String/0003LongestSupstringWithoutRepeatingCharacters.js
+++ Leetcode/String/0003LongestSupstringWithoutRepeatingCharacters.js
@@ -21,32 +21,27 @@ Note that the answer must be a substring, "pwke" is a subsequence and not a subs
   @return {number}
  */
 
- 
-var lengthOfLongestSubstring = function(s) {
-  let output = 0;
-  let countLeft = 0;  //declare count to return
-  let countRight = 0;
-    //two pointers, one form each end, creating new objects every time to check for each repeated char.
-    // if the chars left on the pass are less than the count stop. 
-    //return the count
-  let fromLeft = {};
-  let fromRight = {};
 
-  for (let i=0; i<s.length; i++ ) {
-    let index = i;
-    while (index < s.length && ( Object.keys(fromLeft).length === 0 || !fromLeft[s[index]])) {
-      fromLeft[s[index]] = true;
-      countLeft++;
-      index++;
-    }
-    if (countLeft > output) {
-      output = countLeft;
-    }
-    fromLeft = {};
-    countLeft = 0;
+var lengthOfLongestSubstring = function(s) {
+  let len = s.length;
+  if (len === 0 || len ===1 ) {             //  if the string is empty or just one return the length of it
+    return len;
   }
-  return output;
+  let longest = 1;                          //  length starts at one
+  let start = 0;                            //  start pointer will increase as we move thru the string finding matching characters
+  
+  for (let i=0; i<len; i++ ) {              //  index of for loop will point at the char at front
+    const curr = s[i];                    
+    const index = s.indexOf(curr, start);   //  indexOf where current is the character to search and start is the index at which to start the search for
+    if ( index >= start && index < i) {     //  this checks to see if the index of the current char is less than i, 
+                                            //    if it is less than i, it means that the character appeared before, and we should increase the start point
+      start = index + 1;                    //    for indexOf to start checking at
+    } else {
+      longest = Math.max(longest, i - start +1);  //  this will compare the longest count yet to the difference between the i and the current start point
+    }                                             //    returning the largest one. 
+  }
+  return longest;
 };
 
-// console.log(lengthOfLongestSubstring("abcabcbb"));
-// console.log(lengthOfLongestSubstring("pwwkew"));
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
