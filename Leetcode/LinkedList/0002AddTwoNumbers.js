281549f0bfe589078e8b7da57151e94c38d273d5
tonaramos <gtonaramos@gmail.com>
2018-11-27 00:28:28 -0800

    Solution with comments.

--- Leetcode/LinkedList/0002AddTwoNumbers.js
+++ Leetcode/LinkedList/0002AddTwoNumbers.js
@@ -10,61 +10,41 @@
   @param {ListNode} l2
   @return {ListNode}
  */
-/*
-two non empty linked list representing two non-negative numbers. 
-digists are stored in reverse order 
-each node contains a single digit
-Add the two numbers and return it as a linked list
 
-
-ListNode { val: 2, next: ListNode { val: 4, next: ListNode { val: 3, next: null } } }
-ListNode { val: 5, next: ListNode { val: 6, next: ListNode { val: 4, next: null } } }
-*/
-
+function ListNode (val) {
+  this.val = val;
+  this.next = null;
+}
 
 var addTwoNumbers = function(l1, l2) {
   
-  const listReader = function (linkList, arr) {
-    let outputArray = arr ? arr : [];
-    
-    outputArray.push(linkList.val);
-    if (linkList.next) {
-      return listReader(linkList.next, outputArray);
-    } else {
-      return outputArray;
+  let pointerOne = l1;   //these are pointers and the nodes that will be overwritten.
+  let pointerTwo = l2;
+ 
+  const headPointer = new ListNode(null);  // is this the last node to be added?
+  let previousPointer = headPointer;     // the parent node of prev
+  let carry = false;                 // the flag to add one to the next node every time
+  while (pointerOne || pointerTwo) {                              // while recursive loop while one of the two lists still has nodes left
+    let current = pointerOne != null ? pointerOne : pointerTwo;   // this will rewrite one either node to make it part of the new list
+    let val1 = pointerOne != null ? pointerOne.val : 0;           // if the node exist the value is given to val1 and val 2
+    let val2 = pointerTwo != null ? pointerTwo.val : 0;           
+    let val3 = carry ? 1 : 0;                                     // will add one depending on the previous sum
+    let sum = val1 + val2 + val3;
+    if (sum > 9) {                                                // will flag to carry a number
+      sum = sum - 10;
+      carry = true;
+    } else  {
+      carry = false;                                              // reset to false incase previous pass was true
     }
-  };
-
-  let arr1 = listReader(l1);
-  let arr2 = listReader(l2);
-  let sum = parseInt(arr1.reverse().join('')) + parseInt(arr2.reverse().join(''));
-  console.log('sum-> ', sum)
-  let listOutput = null;
-  String(sum).split('').map(item => {
-    // console.log(item);
-    listOutput = { val: item, next: listOutput};
-    return true;
-  });
-  return listOutput;
+    current.val = sum;                    // rewrites current node value
+    previousPointer.next = current;       // rewrites what what the previousPointer is pointing to to the rest of the list with the updated node value
+    previousPointer = previousPointer.next;  // moves the pointer of the current node for loop to the next node
+    if (pointerOne) pointerOne = pointerOne.next;  // these move the pointers to the next node
+    if (pointerTwo) pointerTwo = pointerTwo.next;
+  }
+  if (carry) {                              // once at the end of the list (the start of the number to be returned)
+    previousPointer.next = new ListNode(1); // this adds a node of value one to the previous node. 
+  }
+  return headPointer.next;                  // returns the second link in the list created since the first has no val. 
 };
-// test cases
-  const one = { val: 2, next: { val: 4, next: { val: 3, next: null } } };
-  const two = { val: 5, next: { val: 6, next: { val: 4, next: null } } };
 
-  const three = { val: 1, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 0, next: { val: 1, next: null } } } } } } } } } } } } } } } };
-  const four = { val: 5, next: { val: 5, next: { val: 5, next: null } } };;
-
-  console.log(addTwoNumbers(one, two));     // 7 -> 0 -> 8
-  console.log(addTwoNumbers(three, four));  //  6550000000000001
-
-  /*
-Did not pass a test case for input:
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
-[5,6,4]
-
-Output was:
-[0,3,NaN,NaN,1]
-
-Expected
-[6,6,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
-*/
