6e29f332e149db752619fed035508671ce0739a3
tonaramos <gtonaramos@gmail.com>
2019-01-16 12:32:38 -0800

    Final solution.

--- Leetcode/Array/0027RemoveElement.js
+++ Leetcode/Array/0027RemoveElement.js
@@ -33,7 +33,6 @@ for (int i = 0; i < len; i++) {
 }
 */
 
-
 const removeElement = (nums, val) => {
   let j = 0;
   for (let i=0; i<nums.length; i++) {
@@ -44,23 +43,3 @@ const removeElement = (nums, val) => {
   }
   return j;
 }
-
-/*
-work with two pointers. Keeping track of the j pointer which  only changes if its value is diff than val
-  incase its val is equal to val the pointer stays behind and swaps the next diff num with it while keeping 
-    count of how many non equal val have passed. 
-*/
-
-const test1 = [3,2,2,3];
-const test2 = [0,1,2,2,3,0,4,2];
-const test3 = [4,5];
-const test4 = [7,8];
-const test5 = [4,8,5,8,6];
-const test6 = [2,2,2,2,2,2,3,2];
-console.log(removeElement(test1, 3), 'test1 -> ', test1);
-console.log(removeElement(test2, 2), 'test2 -> ', test2);
-console.log(removeElement(test3, 4), 'test3 -> ', test3);
-console.log(removeElement(test4, 8), 'test4 -> ', test4);
-console.log(removeElement(test5, 8), 'test5 -> ', test5);
-console.log(removeElement(test6, 3), 'test6 -> ', test6);
-
