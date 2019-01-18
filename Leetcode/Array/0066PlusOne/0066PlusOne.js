7d201f4071aa50dfcd704c822df2f603f29f6b72
tonaramos <gtonaramos@gmail.com>
2019-01-17 22:27:07 -0800

    Intitial solution.

---
+++ Leetcode/Array/0066PlusOne/0066PlusOne.js
@@ -0,0 +1,50 @@
+/*
+66. Plus One
+
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list,
+and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+
+Example 2:
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+*/
+
+const plusOne = (arr) => {
+  if (arr[0] === 0) return [1];
+  if (arr[0] === 9 && arr.length === 1) return [1,0];
+  let carrier = 1;
+  let index = arr.length -1;
+
+  while (carrier === 1) {
+    if (index === 0 && arr[0] === 9) {
+      arr[0] = 0;
+      return [1,...arr];
+      break;
+    }
+    if (arr[index] < 9) {
+      arr[index] = arr[index] + carrier;
+      carrier = 0;
+      return arr;
+    }
+    if (arr[index] === 9) {
+      arr[index] = 0;
+      index--;
+    }
+    
+
+  }
+  return arr;
+}
+console.log(plusOne([1,2,3]));
+
+module.exports = plusOne;
--- Leetcode/Array/0066PlusOne/test.js
+++ Leetcode/Array/0066PlusOne/test.js
@@ -1,16 +1,40 @@
-const maxSubArray = require('./0053MaximumSubarray.js');
+const plusOne = require('./0066PlusOne.js');
 
 // test('*TEST NAME*', () => {
 //   expect('*FUNC*'('*args*')).toBe('*result*');
 // });
 
-let input = [-2,1,-3,4,-1,2,1,-5,4];
-let output = 6;
+let input = [1,2,3];
+let output = [1,2,4];
 
-test('Returns a Number', () => {
-  expect(typeof(maxSubArray(input))).toBe('array')
+test('Returns an array', () => {
+  expect(Array.isArray(plusOne(input))).toBe(true)
 });
 
-test('Maximum sum', () => {
-  expect(maxSubArray(input)).toBe(6)
+test('Returns the correct solution for [1,2,3]', () => {
+  expect(plusOne(input)).toEqual(output)
+});
+
+test('Returns the correct solution for [0]', () => {
+  input = [0];
+  output = [1];
+  expect(plusOne(input)).toEqual(output)
+});
+
+test('Returns the correct solution for [9,9,9]', () => {
+  input = [9,9,9];
+  output = [1,0,0,0];
+  expect(plusOne(input)).toEqual(output)
+});
+
+test('Returns the correct solution for [1,0,0,0,0,1,0,9]', () => {
+  input = [1,0,0,0,0,1,0,9];
+  output = [1,0,0,0,0,1,1,0];
+  expect(plusOne(input)).toEqual(output)
+});
+
+test('Returns the correct solution for [1,0,0,0,0,1,0,0,0]', () => {
+  input = [1,0,0,0,0,1,0,0,0];
+  output = [1,0,0,0,0,1,0,0,1];
+  expect(plusOne(input)).toEqual(output)
 });
