0dc4317a3c4153fc064daaff464d0e9b468d8480
tonaramos <gtonaramos@gmail.com>
2019-01-25 20:22:57 -0800

    Final solution.

--- Leetcode/Tree/0110BalancedBinaryTree/0110BalanceBinaryTree.js
+++ Leetcode/Tree/0110BalancedBinaryTree/0110BalanceBinaryTree.js
@@ -1,58 +1,39 @@
-/**
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+/*
+110. Balanced Binary Tree
+Given a binary tree, determine if it is height-balanced.
+For this problem, a height-balanced binary tree is defined as:
+a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example 1:
+Given the following tree [3,9,20,null,null,15,7]:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Return true.
+
+Example 2:
+Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+Return false.
 */
-var isBalanced = function(root, level = 1) {
-  if (!root) return false;
-  const traverse = (node, level = 0) => {
-    if (!node) return level;
-    if (!node.left && !node.right){
-      return level + 1;
-    } else {
-      // console.log(traverse(node.left, level + 1));
-      // console.log(traverse(node.right, level + 1 ));
-      return Math.max(traverse(node.left, level + 1), traverse(node.right, level + 1 ));
-    }
+
+
+const isBalanced = function(root) {
+  const height = (node) => {
+    if (node === null) return 0;
+    let l = height(node.left);
+    let r = height(node.right);
+    if (Math.abs(l-r) > 1 || l === -1 || r === -1) return -1;
+    return Math.max(l, r) + 1;
   }
-  console.log('L-> ', traverse(root.left), 'R-> ', traverse(root.right), '|');
-  
-  return Math.abs(traverse(root.left), traverse(root.right)) < 2; 
+  return height(root) !== -1;
 };
-// console.log
-// let input;
-// // []	
-// console.log('0 level is false RETURN ->', isBalanced(input))
-
-input = {val: 1, left: null, right: null };  	
-// [1]	
-console.log('1 level is true RETURN ->', isBalanced(input))
-
-input = {val: 1, left:{val: 2, left: null, right: null }, right: null };  	
-// [1,2]	
-console.log('2 levels is true RETURN ->', isBalanced(input))
-
-input = {val: 1, left:{val: 2, left: {val: 3, left: null, right: null }, right: {val: 3, left: null, right: null } }, right:{val: 2, left: {val: 3, left: null, right: null }, right: {val:3, left: null, right: null } } };  	
-// [1,2,2,3,3,3,3]	
-console.log('3 levels is False RETURN ->', isBalanced(input))
-
-input = {val: 1, left:{val: 2, left: null, right: null }, right:{val: 2, left: {val: 3, left: null, right: null }, right: {val:3, left: null, right: null } } };  	
-// [1,2,2,null,null,3,3]	
-console.log('3 levels is False RETURN -> ', isBalanced(input))
-
-input = {val: 1, left: {val: 2, left: null, right: null }, right: {val: 2, left: {val: 3, left: {val: 4, left: null, right: null }, right: null }, right: null } };  	
-// [1,2,2,null,null,3,null, 4]	
-console.log('4 levels False RETURN -> ', isBalanced(input));	
-
-
-//  input = {val: 1, left: {val: 2, left: null, right: null }, right: {val: 2, left: {val: 3, left: null, right: {val: 4, left: null, right: null } }, right: null } };  	
-
-// // [1,2,2,null,null,3,null,null,null,null,null,4,null,null]	
-
-
-// input = {val: 1, left: {val: 2, left: null, right: null }, right: {val: 2, left:null}}
-
-// // console.log('     ----- 4 levels A----');	
-
-// // console.log('4 levels asymmetric-> ', isBalanced(input));
-
