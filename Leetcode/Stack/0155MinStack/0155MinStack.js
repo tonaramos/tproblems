dd002e25fc9257f9d247b24bc382b8bc1acb75b9
tonaramos <gtonaramos@gmail.com>
2019-01-30 13:32:46 -0800

    Final solution.

--- Leetcode/Stack/0155MinStack/0155MinStack.js
+++ Leetcode/Stack/0155MinStack/0155MinStack.js
@@ -41,63 +41,27 @@ minStack.getMin();   --> Returns -2.
   /**
  * initialize your data structure here.
  */
-var MinStack = function() {
+//Check previous commit for approach with out using the push/pop array methods.
+
+const MinStack = () => {
+  this.min = [];
   this.stack = [];
-  this.min= null;
 };
-MinStack.prototype.push = function(x) {
-   this.stack.push(x);
-   
-   if (this.min) {
-     this.min = Math.min(this.min, x);
-   }  else {
-     this.min = x;
-   } 
+MinStack.prototype.push = (x) => {
+  this.stack.push(x);
+  if (this.min.length === 0 || x <= this.min[this.min.length - 1]) {
+      this.min.push(x);
+  }
 };
-MinStack.prototype.pop = function() {
-   let removedItem = this.stack.splice(this.stack.length-1);
-   if (!this.stack.length) {
-     this.min = null;
-   } else if (this.min === removedItem[0]) {
-     this.min = this.stack.reduce((curr, acc)=> {
-       return Math.min(curr,acc);
-     } )
-   }
+MinStack.prototype.pop = () => {
+  var x = this.stack.pop();
+  if (x === this.min[this.min.length - 1]) {
+      this.min.pop();
+  }
 };
-MinStack.prototype.top = function() {
- return this.stack[this.stack.length-1];  
+MinStack.prototype.top = () => {
+  return this.stack[this.stack.length - 1];
 };
-MinStack.prototype.getMin = function() {
-   return this.min;
+MinStack.prototype.getMin = () => {
+  return this.min[this.min.length - 1];
 };
-
-
-// Your MinStack object will be instantiated and called as such:
-var obj = new MinStack();
-obj.push(2147483646);
-obj.push(2147483646);
-obj.push(2147483647);
-obj;
-console.log(obj.top());
-obj.pop();
-obj;
-console.log(obj.getMin());
-obj.pop();
-obj;
-console.log(obj.getMin());
-obj.pop();
-obj;
-obj.push(2147483647);
-obj;
-console.log(obj.top());
-console.log(obj.getMin());
-obj.push(-2147483648);
-obj;
-console.log(obj.top());
-console.log(obj.getMin());
-obj.pop();
-obj;
-console.log(obj.getMin());
-
-// ["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
-// [[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
