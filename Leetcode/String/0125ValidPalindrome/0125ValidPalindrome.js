0ac03ac13586cd01cf750cd4635cfb3495840807
tonaramos <gtonaramos@gmail.com>
2019-01-30 00:14:27 -0800

    Final solution.

--- Leetcode/String/0125ValidPalindrome/0125ValidPalindrome.js
+++ Leetcode/String/0125ValidPalindrome/0125ValidPalindrome.js
@@ -16,7 +16,7 @@ Output: false
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
+const isPalindrome = (s) => {
   if (s.length < 2) return true;
    let temp = s.replace(/[^0-9A-Z]+/gi,'').toLowerCase();
    let p1 = 0;
@@ -27,16 +27,7 @@ var isPalindrome = function(s) {
        return false;
      }
      p1++;
-     p2--
+     p2--;
    }
    return true;
 };
-
-console.log(' null  RESULT-> ', isPalindrome(''));
-console.log(' a  RESULT-> ', isPalindrome('a'));
-console.log(' ra  RESULT-> ', isPalindrome('ar'));
-console.log(' ra  RESULT-> ', isPalindrome('oo'));
-console.log(' race a car  RESULT-> ', isPalindrome('race a car'));
-console.log(' sugus  RESULT-> ', isPalindrome('sugugus'));
-console.log(' sugus  RESULT-> ', isPalindrome('aaaahaaaaaaa'));
-console.log(' "A man, a plan, a canal: Panama"  RESULT-> ', isPalindrome("A man, a plan, a canal: Panama"));
