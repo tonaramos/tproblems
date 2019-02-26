d81f59dcc94d2b6af191cba179d43d02702062db
tonaramos <gtonaramos@gmail.com>
2019-02-25 23:59:20 -0800

    Initial solution.

--- Leetcode/Math/0202HappyNumber/0202HappyNumber.js
+++ Leetcode/Math/0202HappyNumber/0202HappyNumber.js
@@ -17,8 +17,40 @@ Explanation:
 12 + 02 + 02 = 1
 */
 
-var isHappy = function(n) {
-    
-    
-  return
+
+var isHappy = function(n, count = 0) {
+  let dic = {
+    0: 0,
+    1: 1,
+    2: 4,
+    3: 9,
+    4: 16,  //false
+    5: 25,  //false
+    6: 36,
+    7: 49,
+    8: 64,
+    9: 81,  // false
+  };
+  let str = n + '';
+  let sum = 0;
+
+  for (let i=0; i<str.length; i++) {
+    sum += dic[str[i]];
+  
+  }
+  sum
+  if (sum === 0 || sum === 2 || sum === 3 || sum === 4 || sum === 9) {
+    console.log('FALSE BC last sum is -> ', sum, 'at count-> ', count)
+    return false;
+  }
+
+  if (sum === 1) {
+    return true;
+  }
+
+  if (count > 5000) {
+    return false
+  }
+  // console.log(sum)
+  return isHappy(sum, count+1);
 };
