7288f41320219a90f260b9d82e41d629325aee9a
tonaramos <gtonaramos@gmail.com>
2019-02-26 00:27:38 -0800

    Final solution.

--- Leetcode/Math/0202HappyNumber/0202HappyNumber.js
+++ Leetcode/Math/0202HappyNumber/0202HappyNumber.js
@@ -18,39 +18,43 @@ Explanation:
 */
 
 
-var isHappy = function(n, count = 0) {
+const isHappy = (n) => {
+  let str = n + '';
+  let oneDigCheck = {
+    0: false,
+    1: true,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+    7: true,
+    8: false,
+    9: false,
+  };
+
+  if (str.length === 1) {
+    return oneDigCheck[str];
+  }
+
   let dic = {
     0: 0,
     1: 1,
     2: 4,
     3: 9,
-    4: 16,  //false
-    5: 25,  //false
+    4: 16,
+    5: 25,
     6: 36,
     7: 49,
     8: 64,
-    9: 81,  // false
+    9: 81,
   };
-  let str = n + '';
+
   let sum = 0;
 
   for (let i=0; i<str.length; i++) {
     sum += dic[str[i]];
   
   }
-  sum
-  if (sum === 0 || sum === 2 || sum === 3 || sum === 4 || sum === 9) {
-    console.log('FALSE BC last sum is -> ', sum, 'at count-> ', count)
-    return false;
-  }
-
-  if (sum === 1) {
-    return true;
-  }
-
-  if (count > 5000) {
-    return false
-  }
-  // console.log(sum)
-  return isHappy(sum, count+1);
+  return isHappy(sum);
 };
